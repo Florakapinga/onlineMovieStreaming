88b6c354ea795c94088015d43d557db1b6933969
kapinga <erickfloriana@gmail.com>
2025-01-02 06:40:39 -0800

    movie class

--- movieStreaming.cpp
+++ movieStreaming.cpp
@@ -2,3 +2,13 @@
 #include <string>
 #include <vector>
 #include <iomanip>
+class Movie
+{
+private:
+    std::string title;
+    std::string genre;
+    double duration; // Duration in minutes
+
+public:
+    Movie(std::string t, std::string g, double d)
+        : title(t), genre(g), duration(d) {}
